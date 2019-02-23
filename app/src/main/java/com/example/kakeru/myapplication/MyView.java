3c39a77565c8fe05825d69bb6ecf9c9295ef51e0
kakeru <kakerun1123@gmail.com>
2019-02-23 17:38:33 +0900

    三角形(パス)を描いた

--- app/src/main/java/com/example/kakeru/myapplication/MyView.java
+++ app/src/main/java/com/example/kakeru/myapplication/MyView.java
@@ -4,17 +4,20 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.view.View;
 
 public class MyView extends View
 {
     private Paint paint;
+    private Path path;
 
     //コンストラクタ(初期設定)
     public MyView(Context con)
     {
         super(con);
         paint = new Paint();
+        path = new Path();
     }
 
     //描画の処理
@@ -30,15 +33,30 @@ public class MyView extends View
         canvas.drawCircle(300,500,50, paint);
         paint.setColor(Color.GREEN);
         canvas.drawCircle(300,600,50, paint);
-
         //四角
         paint.setColor(Color.argb(255,2,1,2));
         canvas.drawRect(400,800,600,1000,paint);
-
         //線
         paint.setColor(Color.GREEN);
         paint.setStrokeWidth(10);
         canvas.drawLine(0,0, canvas.getWidth(), canvas.getHeight(),paint);
         canvas.drawLine(0, canvas.getHeight(), canvas.getWidth(),0,paint);
+        //三角形(パス)
+        paint.setColor(Color.YELLOW);
+        paint.setStyle(Paint.Style.STROKE);
+        path.moveTo(500,500);
+        path.lineTo(700,900);
+        path.lineTo(400,800);
+        path.lineTo(500,500);
+        canvas.drawPath(path,paint);
+
+        path.reset();
+
+        path.moveTo(0,0);
+        //これいいのか?
+        path.lineTo(0,canvas.getHeight());
+        path.lineTo(canvas.getWidth()/2,canvas.getHeight()/2);
+        path.lineTo(0,0);
+        canvas.drawPath(path,paint);
     }
 }
