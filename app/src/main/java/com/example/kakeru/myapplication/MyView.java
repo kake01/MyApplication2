b3a0c693548ab99fc517807a174071adada0356e
kakeru <kakerun1123@gmail.com>
2019-02-23 16:59:18 +0900

    円を二つ描いた

--- app/src/main/java/com/example/kakeru/myapplication/MyView.java
+++ app/src/main/java/com/example/kakeru/myapplication/MyView.java
@@ -3,14 +3,18 @@ package com.example.kakeru.myapplication;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.View;
 
 public class MyView extends View
 {
+    private Paint paint;
+
     //コンストラクタ(初期設定)
     public MyView(Context con)
     {
         super(con);
+        paint = new Paint();
     }
 
     //描画の処理
@@ -18,6 +22,14 @@ public class MyView extends View
     protected void onDraw(Canvas canvas)
     {
         canvas.drawColor(Color.BLUE);
-    }
 
+        //円
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20);
+        canvas.drawCircle(300,500,50, paint);
+        paint.setColor(Color.GREEN);
+        canvas.drawCircle(300,600,50, paint);
+
+    }
 }
