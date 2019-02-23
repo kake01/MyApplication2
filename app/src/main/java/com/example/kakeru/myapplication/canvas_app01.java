c7022e8caa1c61b919471dac4b91b0e5730acadf
ikuma70 <kakeruww70@gmail.com>
2019-02-23 14:56:51 +0900

    最初のコミット

---
+++ app/src/main/java/com/example/kakeru/myapplication/canvas_app01.java
@@ -0,0 +1,13 @@
+package com.example.kakeru.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class canvas_app01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canvas_app01);
+    }
+}
